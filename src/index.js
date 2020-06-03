fe9873d94a5dae42137bfaa8306d151d2647b306
github username <github email address>
2020-06-02 23:11:52 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ClickityClick from './components/ClickityClick'
+import LightSwitch from './components/LightSwitch'
 
-ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
